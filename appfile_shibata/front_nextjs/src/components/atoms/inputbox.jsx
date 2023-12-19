b12d8df0b95b900f680bc78eefc3e81c0d5f609a
Yu.S <cat.lmc428@gmail.com>
2023-12-19 19:11:04 +0900

    add chart page and MUI

---
+++ appfile_shibata/front_nextjs/src/components/atoms/inputbox.jsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import Input from "postcss/lib/input";
+
+const BInputBox = (props) => {
+  const className = "inline-flex justify-center whitespace-no-wrap rounded-md border px-4 py-2 text-sm leading-5 font-medium bg-white border-gray-300 text-gray-700 hover:text-gray-500 active:bg-gray-50 active:text-gray-800 transition ease-in-out duration-150 ";
+
+  console.log(props.text);
+  const text = props.text;
+
+  return (
+    <Input type="text" className={className}>placeolder</Input>
+  );
+};
+
+export default InputBox;
